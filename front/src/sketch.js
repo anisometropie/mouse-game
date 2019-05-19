a52355e161de217aceee36bd9fed22db57e0d6fb
Valentin Chanas <anisometropie@gmail.com>
2019-05-19 15:20:50 +0200

    remove comment

--- front/src/sketch.js
+++ front/src/sketch.js
@@ -43,12 +43,4 @@ const P5 = new p5(sk => {
       sk.text(u.name, u.x + 12, u.y + 18)
     })
   }
-  //
-  // function otherDrawing(data) {
-  //   noStroke()
-  //   var rouge = map(data.x, 0, 640, 0, 255)
-  //   var bleu = map(data.y, 0, 480, 0, 255)
-  //   fill(data.col[0], data.col[1], data.col[2])
-  //   ellipse(data.x, data.y, 25, 25)
-  // }
 })
