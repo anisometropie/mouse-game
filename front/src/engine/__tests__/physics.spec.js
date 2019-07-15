c5dbca18398d5d2a1473870296a581e4829041ab
Valentin Chanas <anisometropie@gmail.com>
2019-07-16 01:02:53 +0200

    fix tests import and use new constructors

--- front/src/engine/__tests__/physics.spec.js
+++ front/src/engine/__tests__/physics.spec.js
@@ -1,6 +1,6 @@
-import Rectangle from '../Rectangle'
-import Point from '../Point'
-import user from '../user'
+import { Rectangle } from 'objects/Rectangle'
+import Point from 'objects/Point'
+import Circle from 'objects/Circle'
 
 import {
   isPointInRectangle,
@@ -39,56 +39,56 @@ describe('segment intersects circle function', () => {
     const segment = [new Point(0, 0), new Point(100, 0)]
     describe('circle bottom-center', () => {
       it('should return true if segment intersects circle', () => {
-        const circle = new user(null, null, 50, 20, 30)
+        const circle = new Circle(50, 20, 30)
         expect(segmentIntersectsCircle(segment, circle)).toBe(true)
       })
       it('should return false if segment does not intersects circle', () => {
-        const circle = new user(null, null, 50, 20, 10)
+        const circle = new Circle(50, 20, 10)
         expect(segmentIntersectsCircle(segment, circle)).toBe(false)
       })
     })
 
     describe('circle bottom-right, just below B', () => {
       it('should return true if segment intersects circle', () => {
-        const circle = new user(null, null, 100, 20, 30)
+        const circle = new Circle(100, 20, 30)
         expect(segmentIntersectsCircle(segment, circle)).toBe(true)
       })
       it('should return false if segment does not intersects circle', () => {
-        const circle = new user(null, null, 100, 20, 10)
+        const circle = new Circle(100, 20, 10)
         expect(segmentIntersectsCircle(segment, circle)).toBe(false)
       })
     })
 
     describe('circle bottom right, to the right of point B', () => {
       it('should return true if segment intersects circle', () => {
-        const circle = new user(null, null, 120, 20, 40)
+        const circle = new Circle(120, 20, 40)
         expect(segmentIntersectsCircle(segment, circle)).toBe(true)
       })
       it('should return true if segment intersects circle', () => {
-        const circle = new user(null, null, 120, 20, 30)
+        const circle = new Circle(120, 20, 30)
         expect(segmentIntersectsCircle(segment, circle)).toBe(true)
       })
       it('should return false if segment does not intersects circle', () => {
-        const circle = new user(null, null, 120, 20, 25)
+        const circle = new Circle(120, 20, 25)
         expect(segmentIntersectsCircle(segment, circle)).toBe(false)
       })
       it('should return false if segment does not intersects circle', () => {
-        const circle = new user(null, null, 120, 20, 20)
+        const circle = new Circle(120, 20, 20)
         expect(segmentIntersectsCircle(segment, circle)).toBe(false)
       })
       it('should return false if segment does not intersects circle', () => {
-        const circle = new user(null, null, 120, 20, 10)
+        const circle = new Circle(120, 20, 10)
         expect(segmentIntersectsCircle(segment, circle)).toBe(false)
       })
     })
 
     describe('circle right, same level as segment', () => {
       it('should return true if segment intersects circle', () => {
-        const circle = new user(null, null, 120, 0, 30)
+        const circle = new Circle(120, 0, 30)
         expect(segmentIntersectsCircle(segment, circle)).toBe(true)
       })
       it('should return false if segment does not intersects circle', () => {
-        const circle = new user(null, null, 120, 0, 10)
+        const circle = new Circle(120, 0, 10)
         expect(segmentIntersectsCircle(segment, circle)).toBe(false)
       })
     })
@@ -100,23 +100,23 @@ describe('circle Intersects Rectangle function', () => {
     const rectangle = new Rectangle(0, 0, 20, 10)
     describe('circle center inside rectangle', () => {
       it('should return true if circle is completely inside', () => {
-        const circle = new user(null, null, 10, 5, 3)
+        const circle = new Circle(10, 5, 3)
         expect(circleIntersectsRectangle(circle, rectangle)).toBe(true)
       })
       it('should return true if circle is partially inside', () => {
-        const circle = new user(null, null, 10, 9, 3)
+        const circle = new Circle(10, 9, 3)
         expect(circleIntersectsRectangle(circle, rectangle)).toBe(true)
       })
       it('should return true if circle is partially inside', () => {
-        const circle = new user(null, null, 19, 9, 3)
+        const circle = new Circle(19, 9, 3)
         expect(circleIntersectsRectangle(circle, rectangle)).toBe(true)
       })
       it('should return true if circle center is on the edge', () => {
-        const circle = new user(null, null, 20, 5, 3)
+        const circle = new Circle(20, 5, 3)
         expect(circleIntersectsRectangle(circle, rectangle)).toBe(true)
       })
       it('should return true if circle wraps rectangle completely', () => {
-        const circle = new user(null, null, 15, 5, 20)
+        const circle = new Circle(15, 5, 20)
         expect(circleIntersectsRectangle(circle, rectangle)).toBe(true)
       })
     })
@@ -124,45 +124,45 @@ describe('circle Intersects Rectangle function', () => {
     describe('circle center outside rectangle', () => {
       describe('circle to the right-center of rectangle', () => {
         it('should return true if circle intersects rectangle', () => {
-          const circle = new user(null, null, 25, 5, 10)
+          const circle = new Circle(25, 5, 10)
           expect(circleIntersectsRectangle(circle, rectangle)).toBe(true)
         })
         it('should return false if circle does not intersects rectangle', () => {
-          const circle = new user(null, null, 25, 5, 3)
+          const circle = new Circle(25, 5, 3)
           expect(circleIntersectsRectangle(circle, rectangle)).toBe(false)
         })
       })
 
       describe('circle 45° of B vertex', () => {
         it('should return true if circle intersects rectangle', () => {
-          const circle = new user(null, null, 25, 15, 10)
+          const circle = new Circle(25, 15, 10)
           expect(circleIntersectsRectangle(circle, rectangle)).toBe(true)
         })
         it('should return false if circle does not intersects rectangle', () => {
-          const circle = new user(null, null, 25, 15, 6)
+          const circle = new Circle(25, 15, 6)
           expect(circleIntersectsRectangle(circle, rectangle)).toBe(false)
         })
         it('should return false if circle does not intersects rectangle', () => {
-          const circle = new user(null, null, 25, 15, 5)
+          const circle = new Circle(25, 15, 5)
           expect(circleIntersectsRectangle(circle, rectangle)).toBe(false)
         })
       })
 
       describe('circle aligned with one edge', () => {
         it('should return false if circle does not intersects rectangle', () => {
-          const circle = new user(null, null, 20, 20, 5)
+          const circle = new Circle(20, 20, 5)
           expect(circleIntersectsRectangle(circle, rectangle)).toBe(false)
         })
         it('should return false if circle does not intersects rectangle', () => {
-          const circle = new user(null, null, 0, 20, 5)
+          const circle = new Circle(0, 20, 5)
           expect(circleIntersectsRectangle(circle, rectangle)).toBe(false)
         })
         it('should return false if circle does not intersects rectangle', () => {
-          const circle = new user(null, null, 20, -10, 5)
+          const circle = new Circle(20, -10, 5)
           expect(circleIntersectsRectangle(circle, rectangle)).toBe(false)
         })
         it('should return false if circle does not intersects rectangle', () => {
-          const circle = new user(null, null, 0, -10, 5)
+          const circle = new Circle(0, -10, 5)
           expect(circleIntersectsRectangle(circle, rectangle)).toBe(false)
         })
       })
---
+++ front/src/objects/__tests__/Rectangle.spec.js
@@ -0,0 +1,16 @@
+import { Rectangle } from 'objects/Rectangle'
+
+describe('Rectangle class', () => {
+  it('should return its correct vertices coordinates', () => {
+    const rect = new Rectangle(100, 100, 100, 100)
+    const { A, B, C, D } = rect.vertices
+    expect(A.x).toEqual(100)
+    expect(A.y).toEqual(100)
+    expect(B.x).toEqual(200)
+    expect(B.y).toEqual(100)
+    expect(C.x).toEqual(200)
+    expect(C.y).toEqual(200)
+    expect(D.x).toEqual(100)
+    expect(D.y).toEqual(200)
+  })
+})
