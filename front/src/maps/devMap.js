04af9631bdc7ad1f2d8cc0a474f4b15c644f8162
Valentin Chanas <anisometropie@gmail.com>
2019-07-20 15:50:36 +0200

    The devMap that saved me.

--- front/src/maps/devMap.js
+++ front/src/maps/devMap.js
@@ -12,38 +12,42 @@ const spawn = new RectangleBuilder(20, 20, 100, 100)
   .build()
 
 // WALLS
-const walls = [spawn]
-walls.push(new RectangleBuilder(0, 0, 20, 900).makeCollide().build())
-walls.push(new RectangleBuilder(20, 0, 900, 20).makeCollide().build())
-walls.push(new RectangleBuilder(20, 120, 900, 20).makeCollide().build())
+const walls = [
+  spawn,
+  // new RectangleBuilder(0, 0, 20, 900).makeCollide().build(),
+  // new RectangleBuilder(20, 0, 900, 20).makeCollide().build(),
+  // new RectangleBuilder(20, 120, 900, 20).makeCollide().build()
+  new RectangleBuilder(100, 100, 40, 900).makeCollide().build(),
+  new RectangleBuilder(400, 100, 40, 900).makeCollide().build(),
+  new RectangleBuilder(800, 100, 40, 900).makeCollide().build()
+]
 
 // MOVABLE WALLS
-const movableWalls = [...crusher]
-movableWalls.push(
-  new RectangleBuilder(500, 500, 20, 20)
-    .makeMovable()
-    .makeCollide()
-    .withPath([new Point(100, 100), new Point(500, 100), new Point(300, 300)])
-    .withVelocity(2)
-    .build()
-)
+const movableWalls = [
+  // ...crusher,
+  // new RectangleBuilder(500, 500, 20, 20)
+  //   .makeMovable()
+  //   .makeCollide()
+  //   .withPath([new Point(100, 100), new Point(500, 100), new Point(300, 300)])
+  //   .withVelocity(2)
+  //   .build()
+]
 
 // TRAPS
-const traps = []
-traps.push(
-  new CheckerboardBuilder(600, 20)
-    .withSize(5, 1)
-    .withCellSize(50, 100)
-    .withTiming(new Interval('[0, 500['), new Interval('[1000, 1500]'))
-    .withCycleDuration(2000)
-    .build(),
-  new CheckerboardBuilder(200, 20)
-    .withSize(5, 1)
-    .withCellSize(50, 100)
-    .withTiming(new Interval('[0, 500['), new Interval('[1000, 1500]'))
-    .withCycleDuration(2000)
-    .build()
-)
-traps.push(new RectangleBuilder(20, 140, 800, 20).makeKill().build())
+const traps = [
+  // new CheckerboardBuilder(600, 20)
+  //   .withSize(5, 1)
+  //   .withCellSize(50, 100)
+  //   .withTiming(new Interval('[0, 500['), new Interval('[1000, 1500]'))
+  //   .withCycleDuration(2000)
+  //   .build(),
+  // new CheckerboardBuilder(200, 20)
+  //   .withSize(5, 1)
+  //   .withCellSize(50, 100)
+  //   .withTiming(new Interval('[0, 500['), new Interval('[1000, 1500]'))
+  //   .withCycleDuration(2000)
+  //   .build(),
+  // new RectangleBuilder(20, 140, 800, 20).makeKill().build()
+]
 
 export default { spawn, walls, movableWalls, traps }
