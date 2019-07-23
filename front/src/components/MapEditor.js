37873f758f4027965ef7a38a676230de23e65ede
Valentin Chanas <anisometropie@gmail.com>
2019-07-23 17:42:41 +0200

    drawing rectangle

--- front/src/components/MapEditor.js
+++ front/src/components/MapEditor.js
@@ -3,7 +3,12 @@ import { get } from 'lodash'
 
 import { fpsCounter } from 'utils/FpsCounter'
 import { setPointerLock } from 'engine/pointerLock'
-import { getMousePos, pixelRatio, pixelToGrid } from 'utils/canvas'
+import {
+  getMousePos,
+  pixelRatio,
+  pixelToGrid,
+  computeRectangle
+} from 'utils/canvas'
 import {
   circleIntersectsRectangle,
   segmentIntersectsCircle,
@@ -26,33 +31,91 @@ import { loadMap } from 'utils/maps'
 export const WIDTH = window.innerWidth
 export const HEIGHT = window.innerHeight
 
-let gridPosition = { x: 0, y: 0 }
-let currentWorld = { walls: [], movableWalls: [], traps: [] }
-const currentTool = { size: 40 }
-
 class MapEditor extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      gridPosition: null,
+      currentWorld: { walls: [], movableWalls: [], traps: [] },
+      currentTool: { size: 40 },
+      shapeBeingDrawn: null
+    }
     this.canvas = React.createRef()
     this.ctx = null
   }
 
   componentDidMount() {
     this.ctx = this.canvas.current.getContext('2d')
-    this.canvas.current.onmousemove = this.mouseMoved
     window.requestAnimationFrame(this.draw)
   }
 
   mouseMoved = event => {
+    const {
+      currentTool: { size }
+    } = this.state
     const mouse = getMousePos(this.canvas.current, event)
-    Object.assign(gridPosition, pixelToGrid(mouse, currentTool.size))
+    this.setState({ gridPosition: pixelToGrid(mouse, size) })
+  }
+
+  mouseOut = () => {
+    this.setState({ gridPosition: null })
+  }
+
+  mouseDown = () => {
+    const { gridPosition } = this.state
+    this.setState({ shapeBeingDrawn: gridPosition })
+  }
+
+  mouseUp = () => {
+    const {
+      currentTool: { size },
+      currentWorld,
+      gridPosition,
+      shapeBeingDrawn
+    } = this.state
+    const { x, y, width, height } = computeRectangle(
+      shapeBeingDrawn,
+      gridPosition,
+      size
+    )
+    this.setState({
+      currentWorld: {
+        ...currentWorld,
+        walls: [
+          ...currentWorld.walls,
+          new RectangleBuilder(x, y, width, height).build()
+        ]
+      },
+      shapeBeingDrawn: null
+    })
   }
 
   draw = () => {
-    const { size } = currentTool
+    const {
+      currentWorld,
+      currentTool: { size },
+      gridPosition,
+      shapeBeingDrawn
+    } = this.state
     window.requestAnimationFrame(this.draw)
     this.ctx.clearRect(0, 0, WIDTH, HEIGHT)
-    this.ctx.fillRect(size * gridPosition.x, size * gridPosition.y, size, size)
+    // CURSOR
+    if (gridPosition && !shapeBeingDrawn) {
+      this.ctx.fillRect(
+        size * gridPosition.x,
+        size * gridPosition.y,
+        size,
+        size
+      )
+    }
+    if (gridPosition && shapeBeingDrawn) {
+      const { x, y, width, height } = computeRectangle(
+        shapeBeingDrawn,
+        gridPosition,
+        size
+      )
+      this.ctx.fillRect(x, y, width, height)
+    }
     this.ctx.fillText(fpsCounter.fps, 1000, 20)
     currentWorld.walls.forEach(w => {
       w.display(this.ctx)
@@ -69,7 +132,16 @@ class MapEditor extends React.Component {
   render() {
     return (
       <>
-        <canvas ref={this.canvas} id="canvas" width={WIDTH} height={HEIGHT} />
+        <canvas
+          onMouseMove={this.mouseMoved}
+          onMouseLeave={this.mouseOut}
+          onMouseDown={this.mouseDown}
+          onMouseUp={this.mouseUp}
+          ref={this.canvas}
+          id="canvas"
+          width={WIDTH}
+          height={HEIGHT}
+        />
       </>
     )
   }
--- front/src/utils/canvas.js
+++ front/src/utils/canvas.js
@@ -1,5 +1,7 @@
 import { get, mapValues } from 'lodash'
 
+export const pixelRatio = get(window, 'devicePixelRatio', 1)
+
 export function getMousePos(canvas, evt) {
   var rect = canvas.getBoundingClientRect()
   return {
@@ -12,4 +14,10 @@ export function pixelToGrid(point, ratio) {
   return mapValues(point, n => Math.floor(n / ratio))
 }
 
-export const pixelRatio = get(window, 'devicePixelRatio', 1)
+export function computeRectangle(point1, point2, size) {
+  const x = Math.min(point1.x, point2.x) * size
+  const y = Math.min(point1.y, point2.y) * size
+  const width = size + Math.abs(point1.x - point2.x) * size
+  const height = size + Math.abs(point1.y - point2.y) * size
+  return { x, y, width, height }
+}
