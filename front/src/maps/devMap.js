516e3cbc958f942a86579e4f48b7a1d9016c10dd
Valentin Chanas <anisometropie@gmail.com>
2020-09-14 15:21:21 +0200

    dev map test

--- front/src/maps/devMap.js
+++ front/src/maps/devMap.js
@@ -14,40 +14,40 @@ const spawn = new RectangleBuilder(20, 20, 100, 100)
 // WALLS
 const walls = [
   spawn,
-  // new RectangleBuilder(0, 0, 20, 900).makeCollide().build(),
-  // new RectangleBuilder(20, 0, 900, 20).makeCollide().build(),
-  // new RectangleBuilder(20, 120, 900, 20).makeCollide().build()
-  new RectangleBuilder(100, 100, 40, 900).makeCollide().build(),
-  new RectangleBuilder(400, 100, 40, 900).makeCollide().build(),
-  new RectangleBuilder(800, 100, 40, 900).makeCollide().build()
+  new RectangleBuilder(0, 0, 20, 900).makeCollide().build(),
+  new RectangleBuilder(20, 0, 900, 20).makeCollide().build(),
+  new RectangleBuilder(20, 120, 900, 20).makeCollide().build()
+  // new RectangleBuilder(100, 100, 40, 900).makeCollide().build(),
+  // new RectangleBuilder(400, 100, 40, 900).makeCollide().build(),
+  // new RectangleBuilder(800, 100, 40, 900).makeCollide().build()
 ]
 
 // MOVABLE WALLS
 const movableWalls = [
-  // ...crusher,
-  // new RectangleBuilder(500, 500, 20, 20)
-  //   .makeMovable()
-  //   .makeCollide()
-  //   .withPath([new Point(100, 100), new Point(500, 100), new Point(300, 300)])
-  //   .withVelocity(2)
-  //   .build()
+  ...crusher,
+  new RectangleBuilder(500, 500, 20, 20)
+    .makeMovable()
+    .makeCollide()
+    .withPath([new Point(100, 100), new Point(500, 100), new Point(300, 300)])
+    .withVelocity(2)
+    .build()
 ]
 
 // TRAPS
 const traps = [
-  // new CheckerboardBuilder(600, 20)
-  //   .withSize(5, 1)
-  //   .withCellSize(50, 100)
-  //   .withTiming(new Interval('[0, 500['), new Interval('[1000, 1500]'))
-  //   .withCycleDuration(2000)
-  //   .build(),
-  // new CheckerboardBuilder(200, 20)
-  //   .withSize(5, 1)
-  //   .withCellSize(50, 100)
-  //   .withTiming(new Interval('[0, 500['), new Interval('[1000, 1500]'))
-  //   .withCycleDuration(2000)
-  //   .build(),
-  // new RectangleBuilder(20, 140, 800, 20).makeKill().build()
+  new CheckerboardBuilder(600, 20)
+    .withSize(5, 1)
+    .withCellSize(50, 100)
+    .withTiming(new Interval('[0, 500['), new Interval('[1000, 1500]'))
+    .withCycleDuration(2000)
+    .build(),
+  new CheckerboardBuilder(200, 20)
+    .withSize(5, 1)
+    .withCellSize(50, 100)
+    .withTiming(new Interval('[0, 500['), new Interval('[1000, 1500]'))
+    .withCycleDuration(2000)
+    .build(),
+  new RectangleBuilder(20, 140, 800, 20).makeKill().build()
 ]
 
-export default { spawn, walls, movableWalls, traps }
+export default { spawn, walls, movableWalls, traps, checkpoints: [] }
